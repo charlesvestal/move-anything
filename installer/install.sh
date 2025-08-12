588433c9c065d959bb5070633e7df75b81dcf269
bobbydigitales <rob@controlzee.com>
2025-08-11 20:57:51 -0700

    Add sets of pages to the installer and add supporting scripts

--- installer/install.sh
+++ installer/install.sh
@@ -6,17 +6,37 @@ cat << 'EOM'
 |_|  |_|\___/ \_/ \___|  \____\___/|_| |_|\__|_|  \___/|_| |____/ \__,_|_|  |_|  \__,_|\___\___|
 
 EOM
-set -x
+
+# uncomment to debug
+# set -x
+
+function yes_or_no {
+    while true; do
+        read -p "$* [y/N]: " yn
+
+        case $yn in
+            [Yy]*) return 0  ;;
+            [Nn]*|"") return 1 ;;
+        esac
+    done
+}
+
+install_pages=true
+url=https://github.com/bobbydigitales/control_surface_move/raw/main/
+
+if [ "$1" = "local" ]; then
+  url="file:/Users/rob/OrbStack/debian/home/rob/Projects/control_surface_move/"
+fi
 
 filename=control_surface_move.tar.gz
 hostname=move.local
 username=ableton
-ssh_ableton="ssh -n $username@$hostname"
+ssh_ableton="ssh -o LogLevel=QUIET -n $username@$hostname"
 
-ssh_root="ssh -n root@$hostname"
+ssh_root="ssh -o LogLevel=QUIET -n root@$hostname"
 
 echo "Downloading build..."
-curl -LO "https://github.com/bobbydigitales/control_surface_move/raw/main/$filename"
+curl -LO "$url$filename"
 echo "Build MD5: `md5sum $filename`"
 
 echo "Connecting via ssh to $ssh_ableton..."
@@ -33,11 +53,13 @@ fi
 scp  -o ConnectTimeout=1 $filename ableton@move.local:.
 $ssh_ableton "tar -xvf ./$filename"
 
+$ssh_ableton "killall MoveLauncher Move MoveOriginal"
 
 $ssh_root cp -aL /data/UserData/control_surface_move/control_surface_move_shim.so /usr/lib/
 $ssh_root chmod u+s /usr/lib/control_surface_move_shim.so
 if $ssh_root "test ! -f /opt/move/MoveOriginal"; then
   $ssh_root mv /opt/move/Move /opt/move/MoveOriginal
+  $ssh_ableton cp /opt/move/MoveOriginal ~/
 fi
 
 $ssh_root chmod +x /data/UserData/control_surface_move/Move.sh
@@ -47,9 +69,26 @@ $ssh_root md5sum /opt/move/Move
 $ssh_root md5sum /opt/move/MoveOriginal
 $ssh_root md5sum /usr/lib/control_surface_move_shim.so
 
+if yes_or_no "Install 'Pages of Sets'?"; then
+
+  if $ssh_ableton "test ! -L ~/UserLibrary"; then
+    echo "Installing pages of sets..."
+    $ssh_ableton "mv ~/UserLibrary ~/UserLibrary_base"
+    
+    $ssh_ableton "/data/UserData/control_surface_move/changePage.sh 0 skipLaunch"
+    
+    $ssh_ableton "cp -r ~/UserLibrary_base/Sets/* ~/UserLibrary_0/Sets/"
+
+    
+  else
+    echo "Pages of Sets already installed!"
+  fi
+else
+    echo "Not installing Pages of Sets"
+fi
+
 echo "Restating Move binary with shim installed..."
 
-$ssh_ableton killall MoveLauncher Move MoveOriginal
 
 $ssh_ableton "nohup /opt/move/MoveLauncher 2>/dev/null 1>/dev/null &" &
 
---
+++ src/changePageRelative.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo ">>>>>>>>>>>>>>>>>>>>>>>>Change Page Relative by $0 $1 $2"
+delta=$1
+current_page=$(echo `ls -la /data/UserData/UserLibrary` | grep -o '[0-9]*$')
+echo $current_page
+new_page=$((current_page + delta))
+
+echo $new_page
+/data/UserData/control_surface_move/changePage.sh $new_page
