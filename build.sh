67c6922be73b8a2be5e48f4c5130ed0407516e9a
Jez Kabanoff <thesleepless@gmail.com>
2025-08-24 01:45:51 +1000

    drawing to display and font loading support

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -x
 ./clean.sh    
 mkdir -p ./build/
 echo "Building..."
-gcc  -g -O3 src/control_surface_move.c -o build/control_surface_move -Ilibs/quickjs/quickjs-2025-04-26 -Llibs/quickjs/quickjs-2025-04-26 -lquickjs -lm
-gcc -g3 -shared -fPIC -o build/control_surface_move_shim.so src/control_surface_move_shim.c -ldl
+"${CROSS_PREFIX}gcc" -g -O3 src/control_surface_move.c -o build/control_surface_move -Ilibs/quickjs/quickjs-2025-04-26 -Llibs/quickjs/quickjs-2025-04-26 -lquickjs -lm
+"${CROSS_PREFIX}gcc" -g3 -shared -fPIC -o build/control_surface_move_shim.so src/control_surface_move_shim.c -ldl
 cp ./src/*.js ./src/*.mjs ./src/*.sh ./src/*.py ./build/
