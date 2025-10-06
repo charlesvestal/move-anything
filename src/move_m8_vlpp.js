70220e2ce39208179b9f60eb0f41e11d3dd34335
bobbydigitales <rob@controlzee.com>
2025-10-05 23:59:40 -0400

    Improve audio text printing. Remove text printing from M8 LPP.

--- src/move_m8_vlpp.js
+++ src/move_m8_vlpp.js
@@ -503,9 +503,9 @@ function initLPP() {
     move_midi_external_send(LPPInitSysex);
     showingTop = true;
 
-    clear_screen();
-    print(0, 0, "Move Anything", 1);
-    print(0, 18, "M8 connected", 1);
+    // clear_screen();
+    // print(0, 0, "Move Anything", 1);
+    // print(0, 18, "M8 connected", 1);
 }
 
 function updatePLAYLed() {
@@ -519,10 +519,10 @@ globalThis.init = function () {
     console.log("Move control surface script staring...");
 
 
-    clear_screen();
-    print(0, 0, "Move Anything", 1);
-    print(0, 18, "Waiting for", 1);
-    print(0, 36, "M8 to connect", 1);
+    // clear_screen();
+    // print(0, 0, "Move Anything", 1);
+    // print(0, 18, "Waiting for", 1);
+    // print(0, 36, "M8 to connect", 1);
 }
 
 // globalThis.tick = function () {
