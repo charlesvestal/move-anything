a54bfe539087a812fcb2215aea9a0d6b9b40fba1
Caleb Houlbrooke <caleb@dantech.nz>
2025-12-21 20:29:31 +1300

    Custom MIDI Move Controller

--- src/start_control_surface_move.sh
+++ src/start_control_surface_move.sh
@@ -3,5 +3,6 @@ killall MoveLauncher MoveMessageDisplay Move
 echo "Waiting 1 second for Move binaries to exit..."
 sleep 0.5
 echo "Launching!"
-/data/UserData/control_surface_move/control_surface_move /data/UserData/control_surface_move/move_m8_vlpp.js
+cd /data/UserData/control_surface_move
+./control_surface_move ./move_controller.js
 /opt/move/MoveLauncher
