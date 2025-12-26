8e58a97a4e474f4d9e75f797482b091d070bff14
Caleb Houlbrooke <caleb@dantech.nz>
2025-12-27 11:44:35 +1300

    Added additional key combo (shift-knob8 )to start Move controller

--- src/start_control_surface_move.sh
+++ src/start_control_surface_move.sh
@@ -4,5 +4,5 @@ echo "Waiting 1 second for Move binaries to exit..."
 sleep 0.5
 echo "Launching!"
 cd /data/UserData/control_surface_move
-./control_surface_move ./move_controller.js
+./control_surface_move ./move_m8_vlpp.js
 /opt/move/MoveLauncher
