92cd873aad81244019ff4234bbac5f0b52eb6c45
Caleb Houlbrooke <caleb@dantech.nz>
2025-12-28 21:54:51 +1300

    Updated copy_to_move.sh to use tar.gz file

--- copy_to_move.sh
+++ copy_to_move.sh
@@ -3,7 +3,8 @@ set -x
 echo "Killing control_surface_move process on Move..."
 ssh ableton@move.local killall control_surface_move
 echo "Copying build to Move..."
-scp  -r ./dist/* ableton@move.local:./
+scp control_surface_move.tar.gz ableton@move.local:./
+ssh root@move.local tar -xf /data/UserData/control_surface_move.tar.gz -C /data/UserData
 ssh root@move.local cp -aL /data/UserData/control_surface_move/control_surface_move_shim.so /usr/lib/
 ssh root@move.local chmod u+s /usr/lib/control_surface_move_shim.so
 if ssh root@move.local "test ! -f /opt/move/MoveOriginal"; then
